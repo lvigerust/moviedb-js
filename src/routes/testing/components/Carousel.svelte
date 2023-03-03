0f8b664d8e22397a831ba49c5476a28b3610a92b
Ludvig Vigerust <l.vigerust@gmail.com>
2023-03-03 01:27:16 +0100

    - Finally

--- src/routes/testing/components/Carousel.svelte
+++ src/routes/testing/components/Carousel.svelte
@@ -31,33 +31,33 @@
 </script>
 
 <div
-	class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[33vw] after:shrink-0 after:w-[33vw]"
+	class="flex overflow-x-hidden gap-6 snap-x snap-mandatory before:shrink-0 before:w-[33.3vw] after:shrink-0 after:w-[33.3vw] scroll-smooth"
 >
 	{#each data as movie, index}
-		<div id={getItemId(index)} class="shrink-0 snap-center">
+		<div id={getItemId(index)} class="shrink-0 snap-center rounded-lg">
 			<img
-				class="w-full h-80 object-contain"
+				class="w-full h-80 object-contain rounded-lg"
 				src={'https://image.tmdb.org/t/p/w780/' + movie.backdrop_path}
 				alt={movie.title}
 			/>
-			{index}
+
 			<!-- BUTTONS HERE -->
 			<!-- INDICATOR DOTS HERE -->
 		</div>
 	{/each}
 	<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-		<a on:click={decrease} href={`#${getItemId(prev)}`} class="btn btn-circle scroll-smooth"> ❮ </a>
-		<a on:click={increase} href={`#${getItemId(next)}`} class="btn btn-circle scroll-smooth"> ❯ </a>
+		<a on:click={decrease} href={`#${getItemId(prev)}`} class="btn btn-circle"> ❮ </a>
+		<a on:click={increase} href={`#${getItemId(next)}`} class="btn btn-circle"> ❯ </a>
 	</div>
 </div>
 
-<nav>
+<!-- <nav>
 	{#each data as movie, index}
 		<li>
 			<a on:click|preventDefault={scrollIntoView} href={`#${getItemId(index)}`}>{index}</a>
 		</li>
 	{/each}
-</nav>
+</nav> -->
 
 <!-- PREV/NEXT BUTTONS -->
 <!-- 
