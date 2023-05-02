c2a961b53cad058259b0058d4b868b066e5f1fb1
Ludvig Vigerust <l.vigerust@gmail.com>
2023-05-02 18:48:48 +0200

    bugfix

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -8,7 +8,7 @@ export { default as PosterCard } from './UI/PosterCard.svelte';
 export { default as Search } from './Search.svelte';
 export { default as WatchProviders } from './WatchProviders.svelte';
 
-export { default as Season } from '../routes/tv/[tvId]/Season.svelte';
+export { default as Season } from '../../routes/tv/[tvId]/Season.svelte';
 
 export { default as HeroCarousel } from './carousels/HeroCarousel.svelte';
 export { default as PosterCarousel } from './carousels/PosterCarousel.svelte';
