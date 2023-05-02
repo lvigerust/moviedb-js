8638394a14fda8c8b254fff94551e41d6cda9d1b
Ludvig Vigerust <l.vigerust@gmail.com>
2023-05-02 18:55:41 +0200

    final test hopefully

--- src/routes/movie/[movieId]/+page.svelte
+++ src/routes/movie/[movieId]/+page.svelte
@@ -9,7 +9,7 @@
 <svelte:head>
 	<meta property="og:site_name" content="Stinkmeaner" />
 	<meta property="og:title" content={movieDetails.title} />
-	<meta property="og:image" content={movieDetails.backdrop_path} />
+	<meta property="og:image" content={'https://image.tmdb.org/t/p/original/' + data.backdrop_path} />
 	<meta property="og:image:width" content="1280" />
 	<meta property="og:image:height" content="720" />
 	<meta property="og:description" content={movieDetails.overview} />
